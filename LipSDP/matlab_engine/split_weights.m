94cd1aad53a51223c40de618af7547d622c408e5
Alex Robey <arobey1@seas.upenn.edu>
2019-10-29 00:21:16 -0400

    commented split_weights

--- LipSDP/matlab_engine/split_weights.m
+++ LipSDP/matlab_engine/split_weights.m
@@ -1,10 +1,23 @@
 function [split_w, split_net_dims] = split_weights(weights, net_dims, split_amount)
-    % 
+    % Splits neural network into subnetworks of size {split_amount}
+    %
+    % params:
+    %   * weights: cell             - weights of neural network
+    %   * net_dims: list of ints    - dimensions of layers in network
+    %   * split_amount: int         - size of each subnetwork
+    %
+    % returns:
+    %   * split_w: cell         - weights of each subnetwork
+    %   * split_net_dims: cell  - dimensions of each subnetwork
+    % ---------------------------------------------------------------------
 
+    % number of weights in neural network
     num_weights = size(weights, 2);
-    counter = 1;
-    split_w = {}; split_net_dims = {};
     
+    split_w = {}; 
+    split_net_dims = {};
+    
+    counter = 1;
     for k = 1:split_amount:num_weights
         
         % get ending index of split
